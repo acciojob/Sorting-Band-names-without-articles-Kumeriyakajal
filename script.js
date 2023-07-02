08ae22f4c46c6d202af93e2119f70dbf4db2e355
ketki kumeriya <111123802+Kumeriyakajal@users.noreply.github.com>
2023-07-02 14:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let band=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
 band=band.sort();
 
 console.log(band)
+let ui=document.getElementById('band');
+ui.innerHtml=`
+ {
+ ${band}.map((each)=>{
+ <li>${each}</li>
+ })
+ }
+`
